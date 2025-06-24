48e97bba6c44931ee5d80cfe61399c2a420c4cd5
vr_pramod.b <vr_pramod.b@capitaland.com>
2025-06-24 16:47:23 +0530

    checking plp

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,35 +1,13 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
-import { extractCritical } from "@emotion/server";
+import { Html, Head, Main, NextScript } from "next/document";
 
-export default class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    const page = await ctx.renderPage();
-    const styles = extractCritical(page.html);
-    return {
-      ...initialProps,
-      ...page,
-      styles: (
-        <>
-          {initialProps.styles}
-          <style
-            data-emotion-css={styles.ids.join(" ")}
-            dangerouslySetInnerHTML={{ __html: styles.css }}
-          />
-        </>
-      ),
-    };
-  }
-
-  render() {
-    return (
-      <Html lang="en">
-        <Head />
-        <body className="antialiased">
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body className="antialiased">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
